9e52c26956f94f05f743fd1e805dd2ed697cf5d8
MrRobot4444 <91490007+MrRobot4444@users.noreply.github.com>
2022-03-25 18:28:36 +0100

    Delete update.php

--- update.php
+++
@@ -1,8 +0,0 @@
-<?php
-     $dir =   $_POST["dir"];
-     $speed = $_POST["speed"];
-     $batt =  $_POST["batt"];
-     $data =  $dir . ',' . $speed . ',' . $batt;
-     $file =  './current.txt';
-     file_put_contents($file, $data);
-   ?>
